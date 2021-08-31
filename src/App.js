75e040ba390e6311c6abe89b8634f22364c64bbe
Boubou <abdel@red.com>
2021-08-31 22:27:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { sampleText } from "./sampleText";
+import React, { useState, useEffect } from "react";
+import marked from "marked";
 
 function App() {
+  const [text, setText] = useState(localStorage.getItem("text") || sampleText);
+
+  function handleChange(e) {
+    const txt = e.target.value;
+    setText(txt);
+  }
+
+  const renderText = (text) => {
+    const __html = marked(text, { sanitize: true });
+    return { __html };
+  };
+
+  useEffect(() => {
+    localStorage.setItem("text", text);
+  }, [text]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea
+            onChange={handleChange}
+            className="form-control"
+            rows="35"
+            value={text}
+          ></textarea>
+        </div>
+        <div className="col-sm-6">
+          <div dangerouslySetInnerHTML={renderText(text)}></div>
+        </div>
+      </div>
     </div>
   );
 }
